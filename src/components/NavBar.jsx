c828d98e6288c0a25ad8d832df991a1ae5a6e2aa
Milagros Gardiol <gardiol72@gmail.com>
2022-09-02 16:15:25 -0300

    Remodanding

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,10 +5,12 @@ const NavBar = () => {
     <div>
         <div className="navbar bg-base-100">
           <div className="flex-1">
+            <img className="btn btn-ghost normal-case text-xl" src="https://www.gensh.in/fileadmin/Database/items/Currency/wish_Primogem.png" alt="Primogem" />
             <a className="btn btn-ghost normal-case text-xl">Genshin Impact Store</a>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
                   <li><a>Characters</a></li>
+                  <li><a>Weapons</a></li>
                   <li><a>About Us</a></li>
                 </ul>
             </div>
